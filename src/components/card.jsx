652bb8186d985a818d8106a17306ed4ac19d8822
shaban <146794146+sarzhh@users.noreply.github.com>
2025-06-20 23:34:23 +0000

    fetch gifs with an api

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,8 @@
 export function Card({card}){
 return <>
   <div className="card" style={{border: '2px black solid'}}>
-    <div className="card-image">{card.img} </div>
-    <p className="card-name">{card.name} </p>
+    <div className="card-image"> <img src={card.image} alt={card.alt_text} /> </div>
+    <p className="card-name">{card.title} </p>
   </div>
 </>
 
--- src/components/cardBoard.jsx
+++ src/components/cardBoard.jsx
@@ -1,19 +1,42 @@
+
 import { Card } from "./card"
+import { useState, useEffect } from "react"
 export function CardBoard(){
-    const mockData = [
+    /*const mockData = [
         {name: "alfa", img:"alfa.jpg"},
         {name: "fareeda", img:"fareeda.jpg"},
         {name: "fareed", img:"fareed.jpg"},
         {name: "ihsaan", img:"ihsaan.jpg"},
         {name: "aliyah", img:"aliyah.jpg"},
         {name: "adiza", img:"adiza.jpg"}
-    ]
+    ]*/
+
+    const [gifs, setGif] = useState([])
+
+    useEffect(()=>{
+      fetch('https://api.giphy.com/v1/gifs/search?q=dogs&limit=10&api_key=HbgCUM3NGlo409VPr8b9Ze5PTP5Z21tr')
+      .then(response => response.json())
+      /*set gif in state*/
+      .then(gifs => {
+        const myGifs = gifs.data.map(gif => ({
+            id: gif.id,
+            title: gif.title,
+            image: gif.images.original.url,
+            alt: gif.alt_text
+        }))
+        setGif(myGifs)
+      }
+        )
+    }, [])
+
+   
+    
 
     return(
         <>
         <div className="card-Board">
-            {mockData.map((data, index)=>(
-                <Card key={index} card={data}/>
+            {gifs.map((gif)=>(
+                <Card key={gif.id} card={gif}/>
             ))}
         </div>
         </>
